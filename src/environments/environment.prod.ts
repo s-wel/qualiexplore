cfa31db5d767bee4d87e90f2d164b4564ac23d34
wel <wel@biba.uni-bremen.de>
2022-11-17 11:53:23 +0100

    added socketUrlApi info in environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  socketUrlApi : "http://rasaProdHost:rasaPort"
+  socketUrlApi : "http://i4q-dev.ikap.biba.uni-bremen.de:5055"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  socketUrlApi : "http://rasaDevHost:rasaPort"
+  socketUrlApi : "http://i4q-dev.ikap.biba.uni-bremen.de:5055"
 };
 
 /*
