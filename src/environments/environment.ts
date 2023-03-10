f250f75ea24496bce85c6c6aa285d3538ae76bb1
wel <wel@biba.uni-bremen.de>
2023-03-10 14:09:16 +0100

    added comment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  socketUrlApi : "http://localhost:5005/",
+  socketUrlApi : "http://localhost:5005/", // URL of the Rasa chatbot endpoint.
   botName: "QualiExplore assistant",
   authApi : "http://localhost:5000/graphql", // URL of the Apollo server that interacts with the MongoDB (has an integrated GraphQL server).
   graphApi: "http://localhost:4000/graphql"  // URL of the Apollo server that interacts with Neo4j.
