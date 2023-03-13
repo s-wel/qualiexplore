32f8afafa45c57acdb6f26feb9d2043efbcc6841
wel <wel@biba.uni-bremen.de>
2023-03-13 16:14:37 +0100

    added nginx routes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  socketUrlApi : "http://rasa-production-qualiexplore:5005/", // URL of the Rasa chatbot endpoint.
+  socketUrlApi : "http://localhost/bot", // URL of the Rasa chatbot endpoint.
   botName: "QualiExplore assistant", // TODO check if this is still needed
-  authApi : "http://apollo-auth:5000/graphql", // URL of the Apollo server that interacts with the MongoDB (has an integrated GraphQL server).
-  graphApi: "http://apollo-server-qualiexplore:4000/graphql"  // URL of the Apollo server that interacts with Neo4j.
+  authApi : "http://localhost/auth", // URL of the Apollo server that interacts with the MongoDB (has an integrated GraphQL server).
+  graphApi: "http://localhost/knowledge"  // URL of the Apollo server that interacts with Neo4j.
 };
