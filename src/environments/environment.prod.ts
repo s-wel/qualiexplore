2955e112e53612e880c911b873977e1037914fd7
wel <wel@biba.uni-bremen.de>
2023-03-14 14:15:36 +0100

    added env variable HOSTNAME

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  socketUrlApi : "http://localhost/bot", // URL of the Rasa chatbot endpoint.
+  socketUrlApi : "http://${HOSTNAME}/bot", // URL of the Rasa chatbot endpoint.
   botName: "QualiExplore assistant", // TODO check if this is still needed
-  authApi : "http://localhost/auth", // URL of the Apollo server that interacts with the MongoDB (has an integrated GraphQL server).
-  graphApi: "http://localhost/knowledge"  // URL of the Apollo server that interacts with Neo4j.
+  authApi : "http://${HOSTNAME}/auth", // URL of the Apollo server that interacts with the MongoDB (has an integrated GraphQL server).
+  graphApi: "http://${HOSTNAME}/knowledge"  // URL of the Apollo server that interacts with Neo4j.
 };
