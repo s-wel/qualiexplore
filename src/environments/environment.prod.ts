e8b7f7df4a1861673936e34e1f4456bf8a58aecb
wel <wel@biba.uni-bremen.de>
2023-03-17 13:43:19 +0100

    updated hostname

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  socketUrlApi : "http://i4q-dev.ikap.biba.uni-bremen.de/bot", // URL of the Rasa chatbot endpoint.
+  socketUrlApi : "http://i4q.ikap.biba.uni-bremen.de/bot", // URL of the Rasa chatbot endpoint.
   botName: "QualiExplore assistant", // TODO check if this is still needed
-  authApi : "http://i4q-dev.ikap.biba.uni-bremen.de/auth", // URL of the Apollo server that interacts with the MongoDB (has an integrated GraphQL server).
-  graphApi: "http://i4q-dev.ikap.biba.uni-bremen.de/knowledge"  // URL of the Apollo server that interacts with Neo4j.
+  authApi : "http://i4q.ikap.biba.uni-bremen.de/auth", // URL of the Apollo server that interacts with the MongoDB (has an integrated GraphQL server).
+  graphApi: "http://i4q.ikap.biba.uni-bremen.de/knowledge"  // URL of the Apollo server that interacts with Neo4j.
 };
